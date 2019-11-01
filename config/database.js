7113d6243541b3c08d8c2bee0406fdf9d278c2c4
wawan sumardi <wawansumardi@outlook.com>
2019-11-01 08:43:10 +0700

    change config pg

--- config/database.js
+++ config/database.js
@@ -6,6 +6,8 @@ const Env = use("Env");
 /** @type {import('@adonisjs/ignitor/src/Helpers')} */
 const Helpers = use("Helpers");
 
+const Url = use('url-parse')
+const DATABASE_URL = new Url(Env.get('DATABASE_URL'))
 
 module.exports = {
   /*
